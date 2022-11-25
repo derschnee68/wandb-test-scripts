3e74e1f24ed891f1add16e92a63248ba539b0b58
EmilEOGG <emil@gelfort.net>
2022-11-25 13:12:19 +0100

    added fake dataset upload

---
+++ upload_dataset.py
@@ -0,0 +1,14 @@
+import wandb
+import pickle
+
+wandb.login()
+
+with wandb.init(project="artifacts-example", job_type="load-data") as run:
+    fake_dataset = {"labels": [0,1],"inputs":[False,True]}
+    raw_data = wandb.Artifact("test-ds", type="dataset")
+
+    with raw_data.new_file("test_ds.pt", mode="wb") as file:
+        pickle.dump(fake_dataset,file)
+
+    run.log_artifact(raw_data)
+
