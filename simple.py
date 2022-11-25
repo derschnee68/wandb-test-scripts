60e3135942c4cdc88ae4f3c611386e5af305c2d0
EmilEOGG <emil@gelfort.net>
2022-11-25 13:01:47 +0100

    Added artefact logging

--- simple.py
+++ simple.py
@@ -9,4 +9,8 @@ wandb.init(project="jupyter-projo")
 for ii in range(30):
   wandb.log({"acc": 1 - 2 ** -ii, "loss": 2 ** -ii})
   time.sleep(0.5)
+
+art = wandb.Artifact("my-object-detector", type="model")
+art.add_file("dump_model.pt")
+wandb.log_artifact(art)
  
